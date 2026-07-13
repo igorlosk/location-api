f03816584a1cc6251f93ef15d374798f34c0b381
igorlosk <igor@losku.ru>
2026-07-13 19:58:41 +0300

    Initial commit

---
+++ src/main/java/com/example/locationapi/LocationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.locationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationApiApplication.class, args);
+    }
+
+}
